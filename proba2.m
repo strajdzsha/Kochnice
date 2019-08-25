9d07bd783ffd8d445b228a1a2551b80c3d7286e0
strajdzsha <53653877+strajdzsha@users.noreply.github.com>
2019-08-25 21:05:50 +0200

    Update proba2.m

--- proba2.m
+++ proba2.m
@@ -57,7 +57,7 @@ for l=1:m
     end
 end
 
-%REŠAVANJE PDE%
+%REÅ AVANJE PDE%
 
 applyBoundaryCondition(model,'dirichlet','Edge',[1,2,3,4],'u',0);
 v=20;
@@ -82,7 +82,7 @@ results1n=zeros(iternum,n);
 
 for i=1:iternum
     
-    for j=1:m%racunanje BYO na osnovu predhodnog resenja/racuna se u tezistima trouglova
+    for j=1:m%racunanje BYO (gradijent Bez po Y) na osnovu predhodnog resenja/racuna se u tezistima trouglova
         nodes=T(:,j);
         Xpom=X(:,i);
         bez=Xpom(nodes');
