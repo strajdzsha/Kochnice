97f916d2b201e1f6958b6be412200e58818bb38f
strajdzsha <53653877+strajdzsha@users.noreply.github.com>
2019-08-27 09:10:49 +0200

    Update PakovanjeRez.m

--- PakovanjeRez.m
+++ PakovanjeRez.m
@@ -6,11 +6,13 @@ AproxSol=scatteredInterpolant(p1(1,:)',p1(2,:)',results.XGradients);
 AproxSolMatrix=AproxSol(I,J);
 PropperSol=scatteredInterpolant(p1(1,:)',p1(2,:)',resultsX(iternum,:)');
 PropperSolMatrix=PropperSol(I,J);
-Bi=scatteredInterpolant(p(1,:)',p(2,:)',X(:,iternum));
+Bi=scatteredInterpolant(p(1,:)',p(2,:)',IndukovanoPolje(:,iternum));
 BiMatrix=Bi(I,J);
 CurrentDensity=PropperSolMatrix+(BiMatrix)*v;
 imagesc(CurrentDensity)
 plot(xq,CurrentDensity(d/2,:))
 grid on
-% hold on
-% plot(xq,V2(d/2,:));
+hold on
+plot(xq,AproxSolMatrix(d/2,:))
+hold on
+plot(xq,v*BiMatrix(d/2,:));
