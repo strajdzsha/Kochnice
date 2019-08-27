49ce24fb4c38735d18947c16167c4fe396cb37f5
strajdzsha <53653877+strajdzsha@users.noreply.github.com>
2019-08-27 09:10:56 +0200

    Update proba2.m

--- proba2.m
+++ proba2.m
@@ -30,34 +30,34 @@ mesh=generateMesh(model,'Hmax',hmax);
 
 %DRNDANJE SA TROUGLOVIMA%
 
-[p1,e1,t]=meshToPet(mesh);
+[p1,e1,t]=meshToPet(mesh);%p1-matrica koordinata svih nodova;e1-nebitno;t-matrica 7*(broj trouglova),sadrzi koordinate nodova koje pripadaju tom trouglu 
 M=size(t(1,:));
 m=M(1,2);%broj trouglova
-O=zeros(2,m);
+TezistaTrouglova=zeros(2,m);
 T=t([1,2,3],:);%prve tri linije matrice t1 predstavljaju nodove u temenima trouglova/matlab nodama naziva i sredista ivica-nama irelevantni podaci
 n1=length(p1);
-n=max(T(:));%broj nodova
-p=zeros(2,n);
+n=max(T(:));%broj nodova (koji su temena trougla, n1 je ukupan broj nodova-pogledati komentar na liniji 37)
+p=zeros(2,n);%nodovi koji su temena trouglova
 for i=1:n
     p(:,i)=p1(:,i);
 end
-S=zeros(1,m);
+PovrsineTrouglova=zeros(1,m);
 
 for i=1:m
     xy=p(:,T(:,i));
     x=xy(1,:);%koordinate nodova
     y=xy(2,:);%-||-
-    O(1,i)=(x(1,1)+x(1,2)+x(1,3))/3;
-    O(2,i)=(y(1,1)+y(1,2)+y(1,3))/3;
+    TezistaTrouglova(1,i)=(x(1,1)+x(1,2)+x(1,3))/3;
+    TezistaTrouglova(2,i)=(y(1,1)+y(1,2)+y(1,3))/3;
     S1=[x',y',[1,1,1]'];
-    S(1,i)=det(S1);
+    PovrsineTrouglova(1,i)=det(S1);
 end
 
-A=cell(n,1);%lista nodova po trouglovima kojima pripadaju
+NodoviPoTrouglovima=cell(n,1);%lista nodova po trouglovima kojima pripadaju
 for l=1:m
     for h=1:3
         node=T(h,l);
-        A{node,1}=[A{node,1} l];
+        NodoviPoTrouglovima{node,1}=[NodoviPoTrouglovima{node,1} l];
     end
 end
 
@@ -68,36 +68,36 @@ v=20;
 mi0=4*pi*10^(-7);
 asigma=57*10^3;
 
-B0z=@(x,y)(3*0.01./(0.01+x.^2+y.^2).^(5/2)-1./(0.01+x.^2+y.^2).^(3/2))*(mi0/4*pi);
-iternum=10;%broj iteracija
-X=zeros(n,iternum);%resenja za Bez
+B0z=@(x,y)(3*0.01./(0.01+x.^2+y.^2).^(5/2)-1./(0.01+x.^2+y.^2).^(3/2))*(mi0/4*pi);%z-komponenta polja dipola 
+iternum=1;%broj iteracija
+IndukovanoPolje=zeros(n,iternum);%resenja za Bez
 
 resultsX=zeros(iternum,n1);
 resultsY=zeros(iternum,n1);
 resultsX(1,:)=results.XGradients;
 resuktsY(1,:)=results.YGradients;
 
-Znamo=Desno(m,resultsY(1,:),resultsX(1,:),O,n,v,B0z,asigma,T,p,S,mi0);
-Alpha=SklapanjeMatrice(n,asigma,mi0,A,p,O,S,v);
-X(:,1)=Alpha\Znamo;
+DesnaStranaJednacine=Desno(m,resultsY(1,:),resultsX(1,:),TezistaTrouglova,n,v,B0z,asigma,T,p,PovrsineTrouglova,mi0);
+Alpha=SklapanjeMatrice(n,asigma,mi0,NodoviPoTrouglovima,p,TezistaTrouglova,PovrsineTrouglova,v);
+IndukovanoPolje(:,1)=Alpha\DesnaStranaJednacine;
 
-BYO=zeros(1,m);%izvod Bez po y
+dBezy=zeros(1,m);%izvod Bez po y
 results1n=zeros(iternum,n1);
 
 for i=1:iternum
     
     for j=1:m%racunanje BYO na osnovu predhodnog resenja/racuna se u tezistima trouglova
         nodes=T(:,j);
-        Xpom=X(:,i);
-        bez=Xpom(nodes');
+        IndukovanoPoljepom=IndukovanoPolje(:,i);
+        bez=IndukovanoPoljepom(nodes');
         x=p(1,nodes);
         y=p(2,nodes);
         XY=[ones(1,3)',x',y'];
-        ABC=XY\bez;
-        BYO(1,j)=ABC(3,1);%koeficijent C je izvod po y
+        ABC=XY\bez;%ova sintaksa sledi iz FEM fromulacije/vrednost neke funkcije se aproksimira kao U=a+b*x+c*y
+        dBezy(1,j)=ABC(3,1);%koeficijent C je izvod po y
     end
     
-    F=scatteredInterpolant(O(1,:)',O(2,:)',BYO');%interpoliranje BYO po celom mesh-u
+    F=scatteredInterpolant(TezistaTrouglova(1,:)',TezistaTrouglova(2,:)',dBezy');%interpoliranje dBezy po celom mesh-u
     f=@(location,state)-F(location.x,location.y)-mi0*v*(75*location.x.^3+(75*location.y.^2-3).*location.x)./(25*(location.x.^2+location.y.^2+0.01).^(7/2));
     
     specifyCoefficients(model,'m',0,'d',0,'c',1,'a',0,'f',f,'face',1);
@@ -109,34 +109,34 @@ for i=1:iternum
     resultsY(i+1,:)=results1.YGradients;
     
     if i<iternum
-        Znamo=Desno(m,resultsY(i+1,:),resultsX(i+1,:),O,n,v,B0z,asigma,T,p,S,mi0);
-        Alpha=SklapanjeMatrice(n,asigma,mi0,A,p,O,S,v);
-        X(:,i+1)=Alpha\Znamo;
+        DesnaStranaJednacine=Desno(m,resultsY(i+1,:),resultsX(i+1,:),TezistaTrouglova,n,v,B0z,asigma,T,p,PovrsineTrouglova,mi0);
+        Alpha=SklapanjeMatrice(n,asigma,mi0,NodoviPoTrouglovima,p,TezistaTrouglova,PovrsineTrouglova,v);
+        IndukovanoPolje(:,i+1)=Alpha\DesnaStranaJednacine;
     end
     
 end
 
 
-function beta=BioSavart(k,K,m,p,O,S,mi0)%k-broj node za koje se poziva beta;K-vektor gustine struje dimenzija 2xm
+function Beta=BioSavart(k,K,m,p,TezistaTrouglova,PovrsineTrouglova,mi0)%k-broj node za koje se poziva beta;K-vektor gustine struje dimenzija 2xm
 Bezk=0;
 for l=1:m
     xk=p(1,k);
     yk=p(2,k);
-    ksil=O(1,l);
-    psil=O(2,l);
+    ksil=TezistaTrouglova(1,l);
+    psil=TezistaTrouglova(2,l);
     dx=xk-ksil;
     dy=yk-psil;
-    Sl=S(1,l);
+    Sl=PovrsineTrouglova(1,l);
     cross=K(1,l).*dy-K(2,l).*dx;
     Bezk=Bezk+cross*Sl/(dx^2+dy^2)^(3/2);
 end
-beta=Bezk*mi0/(4*pi);
+Beta=Bezk*mi0/(4*pi);
 end
 
-function Znamo=Desno(m,FX,FY,O,n,v,B0z,asigma,T,p,S,mi0)%racunanje desne strane (12)
+function DesnaStranaJednacine=Desno(m,FX,FY,TezistaTrouglova,n,v,B0z,asigma,T,p,PovrsineTrouglova,mi0)%racunanje desne strane (12)
 
  K2=zeros(2,m);%ovo je gradijent fi
- Znamo=zeros(n,1);
+ DesnaStranaJednacine=zeros(n,1);
  
  for l=1:m
      nodes=T(:,l);
@@ -148,33 +148,33 @@ function Znamo=Desno(m,FX,FY,O,n,v,B0z,asigma,T,p,S,mi0)%racunanje desne strane 
  K3(1,:)=0;
  
  for l=1:m
-     K3(2,l)=B0z(O(1,l),O(2,l))*v;
+     K3(2,l)=B0z(TezistaTrouglova(1,l),TezistaTrouglova(2,l))*v;
  end
  
  for k=1:n
-     Znamo(k,1)=asigma*(BioSavart(k,-K2,m,p,O,S,mi0)+BioSavart(k,-K3,m,p,O,S,mi0));
+     DesnaStranaJednacine(k,1)=asigma*(BioSavart(k,-K2,m,p,TezistaTrouglova,PovrsineTrouglova,mi0)+BioSavart(k,-K3,m,p,TezistaTrouglova,PovrsineTrouglova,mi0));
  end
  
 end
 
-function Alpha=SklapanjeMatrice(n,asigma,mi0,A,p,O,S,v)
+function Alpha=SklapanjeMatrice(n,asigma,mi0,NodoviPoTrouglovima,p,TezistaTrouglova,PovrsineTrouglova,v)
 
 Alpha=zeros(n,n);
 C=-mi0*v/(12*pi*asigma);
 
 for k=1:n
     for j=1:n
-        B=A{j};
+        B=NodoviPoTrouglovima{j};%trazimo kojim sve trouglovima pripada noda j
         b=length(B);
         for q=1:b
             xk=p(1,j);
             yk=p(2,j);
-            ksil=O(1,B(q));
-            psil=O(2,B(q));
+            ksil=TezistaTrouglova(1,B(q));
+            psil=TezistaTrouglova(2,B(q));
             dx=xk-ksil;
             dy=yk-psil;
             dl=(dx.^2+dy.^2).^(3/2);
-            Sl=S(1,B(q));
+            Sl=PovrsineTrouglova(1,B(q));
             Alpha(k,j)=Alpha(k,j)+dx*Sl/dl;
         end
     end
